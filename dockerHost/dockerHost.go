5ca7f9e3c4f04ccd6a6282d6c0155d6c458e0c42
S-Maciejewski <maciejewski.torun@gmail.com>
2022-09-21 12:17:50 +0200

    Get docker exec result for container via API

--- dockerHost/dockerHost.go
+++ dockerHost/dockerHost.go
@@ -1,8 +1,10 @@
 package dockerHost
 
 import (
+	"bytes"
 	"context"
 	"log"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -29,4 +31,50 @@ func getContainers(cli *client.Client) []types.Container {
 		return nil
 	}
 	return containers
+}
+
+func GetLsForContainer(containerName string) string {
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("Unable to get new docker client: %v", err)
+		return ""
+	}
+	containers := getContainers(dockerClient)
+	for _, container := range containers {
+		log.Println("Container: ", container.Names, " Image: ", container.Image)
+		if strings.Contains(container.Names[0], containerName) {
+			log.Println("Container found")
+			return getExecResultForContainer(dockerClient, container.ID, []string{"ls", "-l"})
+		}
+	}
+	log.Fatalf("Container not found: %v", containerName)
+	return ""
+}
+
+func getExecResultForContainer(cli *client.Client, containerId string, cmd []string) (execResult string) {
+	var execConfig types.ExecConfig = types.ExecConfig{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+	}
+	execId, err := cli.ContainerExecCreate(context.Background(), containerId, execConfig)
+	if err != nil {
+		log.Printf("Unable to create exec: %v", err)
+		return ""
+	}
+	res, _ := cli.ContainerExecAttach(context.Background(), execId.ID, types.ExecStartCheck{})
+	defer res.Close()
+	err = cli.ContainerExecStart(context.Background(), execId.ID, types.ExecStartCheck{})
+	if err != nil {
+		log.Printf("Unable to start exec: %v", err)
+		return ""
+	}
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(res.Reader)
+	if err != nil {
+		log.Printf("Unable to read from exec: %v", err)
+		return ""
+	}
+	execResult = buf.String()
+	return
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func setupRouter(router *mux.Router) {
 	router.Methods("GET").Path("/test").HandlerFunc(GetTest)
 	router.Methods("GET").Path("/images").HandlerFunc(GetImages)
+	router.Methods("GET").Path("/ls/{containerName}").HandlerFunc(GetLsForContainer)
 }
 
 func GetTest(w http.ResponseWriter, r *http.Request) {
@@ -31,6 +32,16 @@ func GetImages(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write(jsonData)
+}
+
+func GetLsForContainer(w http.ResponseWriter, r *http.Request) {
+	log.Println("GetLsForContainer GET called")
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	containerName := vars["containerName"]
+	ls := dockerHost.GetLsForContainer(containerName)
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(ls))
 }
 
 func main() {
